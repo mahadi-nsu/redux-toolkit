8c72d45b0499bbc950cba2f0346fcfb753cf657d
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-05-24 16:45:24 +0600

    redux data is available in component level

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useSelector((state) => {
+    console.log(state.products.items);
+    console.log(state.products.status);
+  });
+  return <div>Redux toolkit</div>;
 }
 
 export default App;
