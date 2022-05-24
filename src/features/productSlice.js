44c4e11c79fda3f097a116f32e23326129a63c13
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-05-24 09:45:52 +0600

    redux toolkit added with a slice

---
+++ src/features/productSlice.js
@@ -0,0 +1,15 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  items: [],
+  status: null,
+};
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+});
+
+export default productSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// store setup
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import productsReducer from "./features/productSlice";
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
